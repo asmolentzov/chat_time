28e4ef30aa02e47bc92ce97e6ce0a206f4f74668
Anna Smolentzov <annasmolentzov@gmail.com>
2019-07-09 16:50:23 -0600

    Updates chat controller with users

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,6 +1,7 @@
 class ChatController < ApplicationController
   
   def show
-    
+    @user_1 = User.find_by(name: params[:user_name])
+    @user_2 = User.find_by(name: params[:other_user_name])
   end
 end
