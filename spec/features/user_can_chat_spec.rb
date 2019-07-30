21e813cba7dcf314f2552c17b2ef583194640d07
Anna Smolentzov <annasmolentzov@gmail.com>
2019-07-29 18:07:19 -0600

    Updates feature test for seeing past messages

--- spec/features/user_can_chat_spec.rb
+++ spec/features/user_can_chat_spec.rb
@@ -6,36 +6,51 @@ describe 'As a user' do
     @user_1 = User.create(name: 'Human')
     @user_2 = User.create(name: 'Also Human')
   end
-  
+
   describe 'on the main site' do
     it 'can start a chat' do
       visit '/'
-      
+
       expect(page).to have_content('Who are you?')
       expect(page).to have_content('Who do you want to chat with?')
       fill_in :user_name, with: @user_1.name
       fill_in :other_user_name, with: @user_2.name
       click_button("Chat!")
       
+      expect(current_path).to eq(chat_path)
       expect(page).to have_content("Hi " + @user_1.name + "! You are now chatting with " + @user_2.name + ".")
     end
   end
-  
+
   describe 'on the chat page' do
-    it 'can see past messages between the chatting users' do
-      message_1 = Message.create(sender: @user_1, receiver: @user_2, content: "Hello friend")
-      message_2 = Message.create(sender: @user_2, receiver: @user_1, content: "Hello, also friend!")
-      message_3 = Message.create(sender: @user_1, receiver: @user_2, content: "How are you?")
-      message_4 = Message.create(sender: @user_2, receiver: @user_1, content: "Spiffy!")
+    before(:each) do
+      @message_1 = Message.create!(sender: @user_1, receiver: @user_2, content: "Hello friend")
+      @message_2 = Message.create!(sender: @user_2, receiver: @user_1, content: "Hello, also friend!")
+      @message_3 = Message.create!(sender: @user_1, receiver: @user_2, content: "How are you?")
+      @message_4 = Message.create!(sender: @user_2, receiver: @user_1, content: "Spiffy!")
+      
       visit '/'
       fill_in :user_name, with: @user_1.name
       fill_in :other_user_name, with: @user_2.name
       click_button("Chat!")
+    end
+    it 'can see past messages between the chatting users' do
+      expect(page).to have_content("#{@user_1.name}: #{@message_1.content}")
+      expect(page).to have_content("#{@user_2.name}: #{@message_2.content}")
+      expect(page).to have_content("#{@user_1.name}: #{@message_3.content}")
+      expect(page).to have_content("#{@user_2.name}: #{@message_4.content}")
+    end
+    it 'can send a new message' do
+      content = 'Hello, friend!'
+      fill_in :message, with: content
+      click_button "Send"
       
-      expect(page).to have_content("#{@user_1.name}: #{message_1.content}")
-      expect(page).to have_content("#{@user_2.name}: #{message_2.content}")
-      expect(page).to have_content("#{@user_1.name}: #{message_3.content}")
-      expect(page).to have_content("#{@user_2.name}: #{message_4.content}")
+      expect(current_path).to eq(chat_path)
+      expect(page).to have_content("#{@user_1.name}: #{content}")
+      message = Message.last
+      expect(message.sender).to eq(@user_1)
+      expect(message.receiver).to eq(@user_2)
+      expect(message.content).to eq(content)
     end
   end
 end
