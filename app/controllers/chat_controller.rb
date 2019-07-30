3a378f0a3d34fdcd620dacded04bf1e1bb6a94d8
Anna Smolentzov <annasmolentzov@gmail.com>
2019-07-29 20:35:25 -0600

    Adds find_or_create_by to chat controller for users

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,8 +1,8 @@
 class ChatController < ApplicationController
   
   def show
-    @user_1 = User.find_by(name: params[:user_name])
-    @user_2 = User.find_by(name: params[:other_user_name])
+    @user_1 = User.find_or_create_by(name: params[:user_name])
+    @user_2 = User.find_or_create_by(name: params[:other_user_name])
     @messages = Message.where(sender: @user_1, receiver: @user_2).or(Message.where(sender: @user_2, receiver: @user_1))
     @message = Message.new
   end
